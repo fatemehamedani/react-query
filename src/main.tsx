0b12d850cb814d0a454aed4c48c4ad4a9f9fce28
fatemehamedani <ftmehmdni1380@gmail.com>
2025-04-24 23:54:52 +0330

    Parameterized Queries

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,10 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
+
 import App from "./App.tsx";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 2,
-      gcTime: 300_000, //5m
-      staleTime: 10 * 1000,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: false,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -8,11 +8,29 @@ const PostList = () => {
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul>
-      {data?.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
+    <>
+      <form className="max-w-sm mx-auto">
+        <label
+          htmlFor="user"
+          className="block mb-2 text-sm font-medium text-gray-800 dark:text-white"
+        ></label>
+        <select
+          id="user"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="">choose user</option>
+          <option value="1">user1</option>
+          <option value="2">user2</option>
+          <option value="3">user3</option>
+        </select>
+      </form>
+
+      <ul>
+        {data?.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 export default PostList;
