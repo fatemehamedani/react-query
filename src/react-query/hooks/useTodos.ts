fdd3a677c762b92edfee4591e9fce8721e19d8f1
fatemehamedani <ftmehmdni1380@gmail.com>
2025-05-04 10:57:19 +0330

    Creating a Custom Mutation Hook

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { CACHE_KEY_TODOS } from "../constants";
+
 
 export interface Todo {
   userId: number;
@@ -15,7 +17,7 @@ const useTodos = () => {
       .then((res) => res.data);
 
   return useQuery<Todo[], Error>({
-    queryKey: ["todos"],
+    queryKey:CACHE_KEY_TODOS ,
     queryFn: fetchTodo,
     staleTime: 10 * 1000,
   });
